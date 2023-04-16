8a16d68f2e41718456c31482b546b3a54496ff2b
Sam Smith <samsmithnz@gmail.com>
2023-04-15 21:51:47 -0400

    Added new test

--- src/MermaidDotNet.Tests/FlowchartCalculationTests.cs
+++ src/MermaidDotNet.Tests/FlowchartCalculationTests.cs
@@ -1,3 +1,5 @@
+using MermaidDotNet.Models;
+
 namespace MermaidDotNet.Tests;
 
 [TestClass]
@@ -13,6 +15,29 @@ public class FlowchartCalculationTests
 
         //Act
         string result = flowchart.CalculateFlowchart(new(), new());
+
+        //Assert
+        Assert.IsNotNull(flowchart);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void SingleNodeFlowchart()
+    {
+        //Arrange
+        string direction = "LR";
+        Flowchart flowchart = new(direction);
+        List<Node> nodes = new()
+        {
+            new("node1", "This is node 1")
+        };
+        string expected = @"flowchart LR 
+    node1[This is node1]
+";
+
+        //Act
+        string result = flowchart.CalculateFlowchart(nodes, new());
 
         //Assert
         Assert.IsNotNull(flowchart);
