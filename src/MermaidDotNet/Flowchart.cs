4a8961d53c97e29ba142d8036dc43de9a688d5c5
Sam Smith <samsmithnz@gmail.com>
2023-09-23 19:36:03 -0400

    Added new subgraphs. Tests all pass so far.

--- src/MermaidDotNet/Flowchart.cs
+++ src/MermaidDotNet/Flowchart.cs
@@ -1,5 +1,6 @@
 ﻿using MermaidDotNet.Models;
 using System.Text;
+using System.Xml.Linq;
 
 namespace MermaidDotNet;
 
@@ -8,6 +9,7 @@ public class Flowchart
     public string Direction { get; set; }
     public List<Node> Nodes { get; set; }
     public List<Link> Links { get; set; }
+    public List<SubGraph>? SubGraphs { get; set; }
 
     /// <summary>
     /// Initialize the flowchart
@@ -15,7 +17,7 @@ public class Flowchart
     /// <param name="direction">Currently accepts LR and TR options</param>
     /// <param name="nodes">A list of nodes</param>
     /// <param name="links">A list of links</param>
-    public Flowchart(string direction, List<Node> nodes, List<Link> links)
+    public Flowchart(string direction, List<Node> nodes, List<Link> links, List<SubGraph>? subGraphs = null)
     {
         if (direction != "LR" && direction != "TD")
         {
@@ -27,6 +29,7 @@ public class Flowchart
         }
         Nodes = nodes;
         Links = links;
+        SubGraphs = subGraphs;
     }
 
     /// <summary>
@@ -38,38 +41,74 @@ public class Flowchart
         StringBuilder sb = new();
         sb.Append("flowchart " + Direction + Environment.NewLine);
 
+        //Add Subgroups, and their nodes and links
+        if (SubGraphs != null)
+        {
+            foreach (SubGraph subGroup in SubGraphs)
+            {
+                sb.Append("    ");
+                sb.Append("subgraph ");
+                sb.Append(subGroup.Name);
+                sb.Append(Environment.NewLine);
+                foreach (Node node in subGroup.Nodes)
+                {
+                    sb.Append(AddNode(node));
+                }
+                foreach (Link link in subGroup.Links)
+                {
+                    sb.Append(AddLink(link));
+                }
+                sb.Append("end ");
+                sb.Append(Environment.NewLine);
+            }
+        }
+
         //Add nodes
         foreach (Node node in Nodes)
         {
-            sb.Append("    ");
-            sb.Append(node.Name);
-            sb.Append(node.OpenShape());
-            sb.Append(node.Text);
-            sb.Append(node.CloseShape());
-            sb.Append(Environment.NewLine);
+            sb.Append(AddNode(node));
         }
 
         //Add links
         foreach (Link link in Links)
         {
-            Node? sourceNode = Nodes.Find(n => n.Name == link.SourceNode);
-            Node? destinationNode = Nodes.Find(n => n.Name == link.DestinationNode);
-            if (sourceNode == null || destinationNode == null)
-            {
-                throw new ArgumentException("Nodes in link connection (" + link.SourceNode + "-->" + link.DestinationNode + ") not found");
-            }
-            sb.Append("    ");
-            sb.Append(sourceNode.Name);
-            sb.Append("--");
-            if (!string.IsNullOrEmpty(link.Text))
-            {
-                sb.Append(link.Text);
-                sb.Append("--");
-            }
-            sb.Append('>');
-            sb.Append(destinationNode.Name);
-            sb.Append(Environment.NewLine);
+            sb.Append(AddLink(link));
         }
+        return sb.ToString();
+    }
+
+    private string AddNode(Node node)
+    {
+        StringBuilder sb = new();
+        sb.Append("    ");
+        sb.Append(node.Name);
+        sb.Append(node.OpenShape());
+        sb.Append(node.Text);
+        sb.Append(node.CloseShape());
+        sb.Append(Environment.NewLine);
+        return sb.ToString();
+    }
+
+    private string AddLink(Link link)
+    {
+        StringBuilder sb = new();
+        Node? sourceNode = Nodes.Find(n => n.Name == link.SourceNode);
+        Node? destinationNode = Nodes.Find(n => n.Name == link.DestinationNode);
+        if (sourceNode == null || destinationNode == null)
+        {
+            throw new ArgumentException("Nodes in link connection (" + link.SourceNode + "-->" + link.DestinationNode + ") not found");
+        }
+        sb.Append("    ");
+        sb.Append(sourceNode.Name);
+        sb.Append("--");
+        if (!string.IsNullOrEmpty(link.Text))
+        {
+            sb.Append(link.Text);
+            sb.Append("--");
+        }
+        sb.Append('>');
+        sb.Append(destinationNode.Name);
+        sb.Append(Environment.NewLine);
         return sb.ToString();
     }
 
