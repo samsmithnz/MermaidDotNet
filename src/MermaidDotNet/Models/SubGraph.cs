3d95b006cb43f33a8d1715ab252ddbb3beb05345
Sam Smith <samsmithnz@gmail.com>
2023-09-24 15:07:50 -0400

    updated direction

--- src/MermaidDotNet/Models/SubGraph.cs
+++ src/MermaidDotNet/Models/SubGraph.cs
@@ -3,6 +3,8 @@
     public class SubGraph
     {
         public string Name { get; set; }
+        //Can be TB, BT, RL, LR (default: LR) (Top/Bottom/Left/Right, respectively)
+        public string Direction { get; set; } = "LR";
         public List<Node> Nodes { get; set; } = new();
         public List<Link> Links { get; set; } = new();
 
