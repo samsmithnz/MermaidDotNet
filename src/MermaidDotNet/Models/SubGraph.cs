4de65e7df8129142acfd6b0833fbd4dded26c0cc
Sam Smith <samsmithnz@gmail.com>
2023-09-24 15:12:23 -0400

    Added direction to code and test

--- src/MermaidDotNet/Models/SubGraph.cs
+++ src/MermaidDotNet/Models/SubGraph.cs
@@ -8,15 +8,17 @@
         public List<Node> Nodes { get; set; } = new();
         public List<Link> Links { get; set; } = new();
 
-        public SubGraph(string name)
+        public SubGraph(string name, string? direction = null)
         {
             Name = name.Replace(" ", "");
+            Direction = direction;
         }
-        public SubGraph(string name, List<Node> nodes, List<Link> links)
+        public SubGraph(string name, List<Node> nodes, List<Link> links, string? direction = null)
         {
             Name = name.Replace(" ", "");
             Nodes = nodes;
             Links = links;
+            Direction = direction;
         }   
     }
 }
