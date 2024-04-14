428173291dfe9c0f7f46d45fb8c87b4ae1c3a427
Sam Smith <samsmithnz@gmail.com>
2024-04-14 19:09:33 -0400

    Updated test to show correctly.

--- src/MermaidDotNet.Tests/FlowchartTests.cs
+++ src/MermaidDotNet.Tests/FlowchartTests.cs
@@ -45,4 +45,26 @@ public class FlowchartTests
             Assert.AreEqual("Direction none is currently unsupported", ex.Message);
         }
     }
+
+
+
+    [TestMethod]
+    public void NodesAddedIncorrectlyFlowchart()
+    {
+        //Arrange
+        try
+        {
+            Flowchart flowchart = new("LR", new(), new());
+            flowchart.Nodes.Add(new("node1", "node1"));
+
+            //Act
+
+            //Assert
+            Assert.IsNotNull(flowchart);
+        }
+        catch (Exception ex)
+        {
+            Assert.AreEqual("The NavigationNodes collection is empty, but Nodes collection is not empty. This is likely an issue because Nodes were added manually instead of as a collection in the FlowChart constructor", ex.Message);
+        }
+    }
 }
--- src/MermaidDotNet/Flowchart.cs
+++ src/MermaidDotNet/Flowchart.cs
@@ -131,9 +131,13 @@ public class Flowchart
     private string AddLink(Link link)
     {
         StringBuilder sb = new();
+        if (NavigationNodes.Count == 0 && Nodes.Count > 1)
+        {
+            throw new ArgumentException("The NavigationNodes collection is empty, but Nodes collection is not empty. This is likely an issue because Nodes were added manually instead of as a collection in the FlowChart constructor");
+        }
         Node? sourceNode = NavigationNodes.Find(n => n.Name == link.SourceNode);
         Node? destinationNode = NavigationNodes.Find(n => n.Name == link.DestinationNode);
-        if (sourceNode == null )
+        if (sourceNode == null)
         {
             throw new ArgumentException("Source node in link connection (" + link.SourceNode + "-->" + link.DestinationNode + ") not found");
         }
