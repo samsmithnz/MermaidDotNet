bcebd882fcca33fe8a9f9cecda10cf5aa064a6ba
Sam Smith <samsmithnz@gmail.com>
2025-02-04 22:05:02 -0500

    Added MVC sample

---
+++ src/MermaidDotNet.MVCWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MermaidDotNet.MVCWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
